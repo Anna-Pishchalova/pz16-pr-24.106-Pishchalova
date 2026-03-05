9f9d387768e768a456eda98ba4f518ac91d51265
Kunukida <annapisalova@gmail.com>
2026-03-05 22:05:21 +0700

    Create code part

--- app/src/main/java/com/example/pz16/MainActivity.java
+++ app/src/main/java/com/example/pz16/MainActivity.java
@@ -1,24 +1,69 @@
 package com.example.pz16;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private EditText editText;
+    private final String SAVED_TEXT = "saved_text";
+    private SharedPreferences sharedPreferences;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        editText = findViewById(R.id.editext);
+        Button btnSave = findViewById(R.id.save);
+        Button btnLoad = findViewById(R.id.load);
+        btnSave.setOnClickListener(this);
+        btnLoad.setOnClickListener(this);
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.save) {
+            saveText();
+        } else if (id == R.id.load) {
+            loadText();
+        }
+    }
+
+    private void saveText() {
+        String text = editText.getText().toString().trim();
+
+        if (text.isEmpty()) {
+            Toast.makeText(this, "Введите текст", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        @SuppressLint("CommitPrefEdits") SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(SAVED_TEXT, text);
+        Toast.makeText(this, "Текст сохранён", Toast.LENGTH_SHORT).show();
+    }
+    private void loadText() {
+        String savedText = sharedPreferences.getString(SAVED_TEXT, "");
+        editText.setText(savedText);
+
+        if (savedText.isEmpty()) {
+            Toast.makeText(this, "Нет сохранённого текста", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Текст загружен", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        saveText();
     }
 }
